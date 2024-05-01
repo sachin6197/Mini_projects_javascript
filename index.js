6a97228b4dc5e6d3e5610577344cf38e73fc189e
sachin6197 <sachinrandive5837@gmail.com>
2024-05-01 22:47:57 +0530

    delayed _to_deploy

---
+++ index.js
@@ -0,0 +1,21 @@
+let grey = document.getElementById("grey");
+let orange = document.getElementById("orange");
+let blue = document.getElementById("blue");
+let yellow = document.getElementById("yellow");
+
+
+grey.addEventListener("click", function (){
+    document.body.style.backgroundColor = "grey"
+});
+
+orange.addEventListener("click", function (){
+    document.body.style.backgroundColor = "orange"
+});
+
+blue.addEventListener("click", function (){
+    document.body.style.backgroundColor = "blue"
+});
+
+yellow.addEventListener("click", function (){
+    document.body.style.backgroundColor = "yellow"
+});
